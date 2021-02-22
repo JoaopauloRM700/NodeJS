3cf1ea0b6e3d1b8f30abdd27493909bd4983f2ac
João Paulo Reis Marques <joaopaulo.rm7@gmail.com>
2021-02-22 14:41:13 -0400

    node

--- nlw-rocketseat/API/src/server.ts
+++ nlw-rocketseat/API/src/server.ts
@@ -1,6 +1,30 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+
+/**
+ * GET => Buscar
+ * POST => Salvar
+ * PUT => Alterar
+ * DELETE => Deletar
+ * PATCH => Alteração específica
+ */
+
+ //http://localhost:3333/users
+ app.get("/",(request, response) => {
+     return response.json({message:"Hello Word - NLW04"});
+ });
+
+ // 1 param => Rota(Recurso API)
+ // 2 param => resquest, response
+
+ app.post("/",(request, response)=>{
+
+    return response.json({
+        message:"os dados foram salvos!"
+    });
+
+ });
 
 app.listen(3333,() => console.log("Server is running!"));
 
